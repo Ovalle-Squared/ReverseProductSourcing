84784c017ee00b3f563630ecb34cc5394d661bc6
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2022-12-19 19:34:26 -0600

    got code working again, added new tasks

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ if __name__ == '__main__':
                     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 '
                     'Safari/537.36', 'Accept-Language': 'en-US'})
     URL = "https://www.amazon.com/Sony-PlayStation-Pro-1TB-Console-4/dp/B07K14XKZH/"
+    #Grab weekly ad url
+    #Search soup for aria-label
+    #Search Lowes listings based on aria-labels
     webpage = requests.get(URL, headers=HEADERS)
     soup = BeautifulSoup(webpage.content, "lxml")
-    get_title(URL)
+    #print(soup)
+    title = get_title(URL)
+    #print(title)
