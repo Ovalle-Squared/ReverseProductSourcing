c300e525036320b362057e8c7837af565072c21a
Johnneisha White <johnneisha.white@gmail.com>
2022-12-26 18:52:08 -0600

    Competitor Product Title

--- main.py
+++ main.py
@@ -14,17 +14,46 @@ def get_title(url):
     title_string = title_value.strip()
     return str(title_string)
 
+#def get_compProductsTitle(storeData)
+
 
 if __name__ == '__main__':
     HEADERS = ({'User-Agent':
                     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 '
                     'Safari/537.36', 'Accept-Language': 'en-US'})
-    URL = "https://www.amazon.com/Sony-PlayStation-Pro-1TB-Console-4/dp/B07K14XKZH/"
+    ### COMPETITOR'S STOREFRONT ###
+    storefrontURL = "https://www.amazon.com/s?me=A3B6E79ULPISFM&marketplaceID=ATVPDKIKX0DER" #eventually improve to take list of URLS
+    storefrontWebpage =  requests.get(storefrontURL, headers=HEADERS)
+    storefrontSoup = BeautifulSoup(storefrontWebpage.content, "lxml")
+    compProducts = storefrontSoup.find('span', attrs={"data-component-type": 's-search-results'})
+   
+    #title#
+    compTitleData = compProducts.find_all('span', attrs= {"class": 'a-size-medium a-color-base a-text-normal'})
+    #print(compTitleData)
+    compProductTitles = []
+    for item in compTitleData:
+        compProductTitles.append(item.string.strip())
+
+    """#print product list
+        for product in compProductTitles:
+            print(product)
+    """
+
+    #price#
+    
+
+    """productURLS_value = productURLs.string
+    productURLs_string = productURLS_value.strip()
+   """
+    
+    """### SEARCH PRODUCT ON AMAZON ### 
+    productURL = "https://www.amazon.com/Sony-PlayStation-Pro-1TB-Console-4/dp/B07K14XKZH/"
     #Grab weekly ad url
     #Search soup for aria-label
     #Search Lowes listings based on aria-labels
-    webpage = requests.get(URL, headers=HEADERS)
-    soup = BeautifulSoup(webpage.content, "lxml")
+    webpage = requests.get(productURL, headers=HEADERS)
+    productSoup = BeautifulSoup(webpage.content, "lxml")
     #print(soup)
-    title = get_title(URL)
-    #print(title)
+    productTitle = get_title(productURL)
+    #print(productTitle)
+    """
