67a2529aa41cf553a0891612624c6e237801f91c
Johnneisha White <johnneisha.white@gmail.com>
2022-12-26 21:23:36 -0600

    get product title, and prices. working on az market prices.

--- main.py
+++ main.py
@@ -6,13 +6,27 @@ import requests
 """
 
 
-def get_title(url):
+def get_title(soup):
     title = soup.find("span", attrs={"id": 'productTitle'})
-    #print(title)
-    title_value = title.string
-    #print(title_value)
-    title_string = title_value.strip()
-    return str(title_string)
+    if(title is None):
+        return "None"
+    else: 
+        #print(title)
+        title_value = title.string
+        #print(title_value)
+        title_string = title_value.strip()
+        return str(title_string)
+
+def get_price(soup):
+    productPrice = soup.find("span", attrs={"id": 'price'})
+    if(productPrice is None):
+      productPrice = soup.find("span", attrs= {"class": 'a-size-base a-color-price offer-price a-text-normal'})
+    if(productPrice is None):
+        return "0"
+    else:
+        productPrice_value = productPrice.string
+        productPrice_string = productPrice_value.strip()
+        return str(productPrice_string)
 
 #def get_compProductsTitle(storeData)
 
@@ -21,39 +35,61 @@ if __name__ == '__main__':
     HEADERS = ({'User-Agent':
                     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 '
                     'Safari/537.36', 'Accept-Language': 'en-US'})
+
     ### COMPETITOR'S STOREFRONT ###
-    storefrontURL = "https://www.amazon.com/s?me=A3B6E79ULPISFM&marketplaceID=ATVPDKIKX0DER" #eventually improve to take list of URLS
+    storefrontURL = "https://www.amazon.com/s?me=AMNY61XEYVLRP&marketplaceID=ATVPDKIKX0DER" #eventually improve to take list of URLS
     storefrontWebpage =  requests.get(storefrontURL, headers=HEADERS)
     storefrontSoup = BeautifulSoup(storefrontWebpage.content, "lxml")
-    compProducts = storefrontSoup.find('span', attrs={"data-component-type": 's-search-results'})
+    compProductsData = storefrontSoup.find('span', attrs={"data-component-type": 's-search-results'})
+    #print(compProductsData)
    
     #title#
-    compTitleData = compProducts.find_all('span', attrs= {"class": 'a-size-medium a-color-base a-text-normal'})
+    compTitleData = compProductsData.find_all('span', attrs= {"class": 'a-size-medium a-color-base a-text-normal'})
     #print(compTitleData)
     compProductTitles = []
-    for item in compTitleData:
-        compProductTitles.append(item.string.strip())
+    for title in compTitleData:
+        compProductTitles.append(title.string.strip())
 
-    """#print product list
-        for product in compProductTitles:
+    #print product list
+        for title in compProductTitles:
             print(product)
-    """
+    
 
+    #URL#
+    compProductURLs = []
+    for url in compProductsData.find_all('a', href=True):
+        if ("javascript:void" not in url['href']) and ("customerReviews" not in url['href']) and ("gp/" not in url['href']) and ("s?i=merchant" not in url['href']) and ("kindle-dbs" not in url['href']) and ("signin?" not in url['href']):
+            result = ("https://www.amazon.com" + url['href'])
+            if(result not in compProductURLs):
+                compProductURLs.append(result)
+     
+    #print product url
+    for url in compProductURLs:
+        print(url)
+    
+    
+    
     #price#
-    
 
-    """productURLS_value = productURLs.string
-    productURLs_string = productURLS_value.strip()
-   """
+    #productURL = "http://www.amazon.com/2023-National-Park-Foundation-Calendar/dp/1728250021/ref=sr_1_1?m=A3B6E79ULPISFM&marketplaceID=ATVPDKIKX0DER&qid=1672105901&s=merchant-items&sr=1-1"
+    productWebpage = requests.get(productURL, headers=HEADERS)
+    productSoup = BeautifulSoup(productWebpage.content, "lxml")
+    if compProductURLs is not None:
+        for productURL in compProductURLs:
+            productWebpage = requests.get(productURL, headers=HEADERS)
+            productSoup = BeautifulSoup(productWebpage.content, "lxml")
+            print(get_title(productSoup) + ", " get_price(productSoup) + ", " + productURL) 
+            # + ", " + productURL)
+
+
     
-    """### SEARCH PRODUCT ON AMAZON ### 
-    productURL = "https://www.amazon.com/Sony-PlayStation-Pro-1TB-Console-4/dp/B07K14XKZH/"
-    #Grab weekly ad url
-    #Search soup for aria-label
-    #Search Lowes listings based on aria-labels
-    webpage = requests.get(productURL, headers=HEADERS)
-    productSoup = BeautifulSoup(webpage.content, "lxml")
-    #print(soup)
-    productTitle = get_title(productURL)
-    #print(productTitle)
-    """
+    ### AMAZON MARKET ### 
+    marketProductURLS = []
+    for url in compProductURLs:
+        index = url.find("ref=sr")
+        marketProductURLS.append(url[:index])
+    
+    for url in marketProductURLS: 
+        productWebpage = requests.get(url, headers=HEADERS)
+        productSoup = BeautifulSoup(productWebpage.content, "lxml")
+        get_price(productSoup)
